fce3a2f6d09eb9924437f85c82472b8e9e9831d1
王晓丽 <wangxiaoli@troila.com>
2019-11-27 14:45:49 +0800

    add pug

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.set("view engine", "pug");
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get((req, res) => {
+  res.sendFile(process.cwd() + "/views/pug/index.html");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
